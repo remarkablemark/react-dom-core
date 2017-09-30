6679e11a5a7b72e15cf2f70f1c7c7ab33bc4b23b
Mark <mark@remarkablemark.org>
2017-09-30 19:26:48 -0400

    feat(property-config): expose react-dom@15 DOM property config
    ARIADOMPropertyConfig:
    https://github.com/facebook/react/blob/v15.6.1/src/renderers/dom/shared/ARIADOMPropertyConfig.js
    
    HTMLDOMPropertyConfig:
    https://github.com/facebook/react/blob/v15.6.1/src/renderers/dom/shared/HTMLDOMPropertyConfig.js
    
    SVGDOMPropertyConfig:
    https://github.com/facebook/react/blob/v15.6.1/src/renderers/dom/shared/SVGDOMPropertyConfig.js

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  ARIADOMPropertyConfig: require('./src/ARIADOMPropertyConfig'),
+  HTMLDOMPropertyConfig: require('./src/HTMLDOMPropertyConfig'),
+  SVGDOMPropertyConfig: require('./src/SVGDOMPropertyConfig')
+};
