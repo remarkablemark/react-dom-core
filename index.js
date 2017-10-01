822812722ab4a4029f5632ef2f138b58a6b2498b
Mark <mark@remarkablemark.org>
2017-09-30 21:55:41 -0400

    feat(lib): copy `react-dom/lib` instead of referencing with require
    npm scripts:
    * copy
      - Copies `./node_modules/react-dom/lib` to `./lib`
    * clean
      - Deletes `./lib`
      - Should be run before `copy`
    
    Add `lib` to .gitignore.
    
    Delete file and directory:
    * index.js
    * src/

--- index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  ARIADOMPropertyConfig: require('./src/ARIADOMPropertyConfig'),
-  HTMLDOMPropertyConfig: require('./src/HTMLDOMPropertyConfig'),
-  SVGDOMPropertyConfig: require('./src/SVGDOMPropertyConfig')
-};
