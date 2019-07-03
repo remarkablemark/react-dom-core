08d4ba2e96c89973103bd430ee3e2906650d37d7
Mark <mark@remarkablemark.org>
2019-07-02 23:56:08 -0400

    feat(react-property): add `index.js` that imports all files in lib
    And add "main" field in `package.json` so the file is included
    during npm publish.

---
+++ packages/react-property/index.js
@@ -0,0 +1,9 @@
+var HTMLDOMPropertyConfig = require('./lib/html');
+var SVGDOMPropertyConfig = require('./lib/svg');
+var isCustomAttribute = require('./lib/is-custom-attribute');
+
+module.exports = {
+  HTMLDOMPropertyConfig: HTMLDOMPropertyConfig,
+  SVGDOMPropertyConfig: SVGDOMPropertyConfig,
+  isCustomAttribute: isCustomAttribute
+};
