182326a0fda6c3e218cb6c8ae52f3774b9187695
Mark <mark@remarkablemark.org>
2019-07-02 23:19:06 -0400

    feat(react-property): add build script for SVG DOM config
    The build script is similar to HTML DOM config except SVG does not
    have boolean or overloaded boolean attributes/properties.

---
+++ packages/react-property/scripts/build-svg.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+const DOMProperty = require('react-dom/lib/DOMProperty');
+const SVGDOMPropertyConfig = require('react-dom/lib/SVGDOMPropertyConfig');
+const { LIB_DIR, SVG_DIR } = require('./constants');
+
+/**
+ * Creates the DOM property map via injection.
+ *
+ * @see https://github.com/facebook/react/blob/15-stable/src/renderers/dom/shared/DOMProperty.js#L57
+ */
+DOMProperty.injection.injectDOMPropertyConfig(SVGDOMPropertyConfig);
+
+// `autofocus` is removed since it's not an applicable attribute for SVG elements
+delete DOMProperty.getPossibleStandardName.autofocus;
+
+/**
+ * Create output directories (if it does not exist).
+ */
+try {
+  fs.mkdirSync(LIB_DIR);
+} catch (error) {
+  // throw error;
+}
+
+try {
+  fs.mkdirSync(SVG_DIR);
+} catch (error) {
+  // throw error;
+}
+
+/**
+ * SVG DOM property config.
+ */
+const attributeToProperty = {};
+
+/**
+ * List of SVG DOM attributes.
+ */
+const attributes = [];
+
+Object.keys(DOMProperty.getPossibleStandardName).forEach(attributeName => {
+  const propertyName = DOMProperty.getPossibleStandardName[attributeName];
+
+  if (attributeName !== propertyName) {
+    attributeToProperty[attributeName] = propertyName;
+  }
+
+  attributes.push(attributeName);
+});
+
+fs.writeFileSync(
+  path.resolve(SVG_DIR, 'attributes.json'),
+  JSON.stringify(attributes)
+);
+
+fs.writeFileSync(
+  path.resolve(SVG_DIR, 'attribute-to-property.json'),
+  JSON.stringify(attributeToProperty)
+);
