480656366107a11bc591e29ec0ec1e0d50184859
Mark <remarkablemark@users.noreply.github.com>
2022-11-12 12:08:03 -0500

    chore(root): add "body-max-line-length" rule
    Warn instead of error so Dependabot PRs don't fail

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,6 +3,7 @@ const { readdirSync } = require('fs');
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'body-max-line-length': [1, 'always', 100],
     'scope-enum': [
       2,
       'always',
