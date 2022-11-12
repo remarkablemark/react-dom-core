b9ae450f7f9d9e2db621bc779aede26a02e7de05
Mark <remarkablemark@users.noreply.github.com>
2022-11-12 12:13:11 -0500

    chore(root): update .commitlintrc.js

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'scope-enum': [
       2,
       'always',
-      ['all', 'deps', 'release', 'root', ...readdirSync('./packages')],
+      ['all', 'deps', 'github', 'release', 'root', ...readdirSync('./packages')],
     ],
     'scope-empty': [2, 'never'],
   },
